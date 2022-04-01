1ec1939f6a10f61f4bb71b6c15c1c8da30240189
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-04-01 12:17:33 +0200

    Auto-Save

--- #1 - Installation, importation et création du Bot/main.py
+++ #1 - Installation, importation et création du Bot/main.py
@@ -1,10 +1,10 @@
-import discord #importation du module de discord
-from discord.ext import commands #importation des commandes via le module "discord.ext"
+import discord # importation du module de discord
+from discord.ext import commands # importation des commandes via le module "discord.ext"
 
-bot = commands.Bot(command_prefix=">") #Vous pouvez choisir le prefix que vous souhaitez. Il suffit de remplacer le ">"
+bot = commands.Bot(command_prefix=">") # Définir le bot et son prefix | Vous pouvez choisir le prefix que vous souhaitez. Il suffit de remplacer le ">"
 
 @bot.event #on créer un évènement qui va se dérouler lors d'une certaine action
 async def on_ready(): #on définit que cette action est "on_ready", ce qui s'activera lors du démarrage du bot
     print("Le bot est démarré!") #On envoie un message de confirmation dans la console
 
-bot.run("NzM2MjE5Njg1MTQxNjEwNTE2.XxroAw.SV2kxn74Wqjug5ukQho7BelhEM8") #Collez votre token à la place de "TOKEN" (les guillemets sont indispensables)
+bot.run("TOKEN") #Collez votre token à la place de "TOKEN" (les guillemets sont indispensables)
